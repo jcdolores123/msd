211977a85f388b9749c2026247686f56265d9860
jcdolores123 <justinecarlodolores@gmail.com>
2020-09-17 12:46:40 +0800

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { DocumentsComponent } from './pages/documents/documents.component'
+import { FileUploadComponent } from './pages/file-upload/file-upload.component'
+import { TextBlastComponent } from './pages/text-blast/text-blast.component'
+import { HomePageComponent } from './pages/home-page/home-page.component'
+
+
+const routes: Routes = [
+    { path: 'text-blast', component: TextBlastComponent },
+    { path: 'file-upload', component: FileUploadComponent },
+    { path: 'documents', component: DocumentsComponent },
+    { path: '', component: HomePageComponent }
+  ];
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
